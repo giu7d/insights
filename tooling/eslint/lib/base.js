2ea3678c6f4b1f1043c82ac0067e5fbcc839515a
Giuseppe Setem <giuseppe7d@gmail.com>
2024-05-03 09:41:39 -0300

    refactor: setup @splitter/tooling-eslint package

---
+++ tooling/eslint/lib/base.js
@@ -0,0 +1,107 @@
+/// <reference types="./types.d.ts" />
+
+import eslint from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+
+/**
+ * All packages that leverage t3-env should use this rule
+ */
+export const restrictEnvAccess = tseslint.config({
+  files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+  rules: {
+    'no-restricted-properties': [
+      'error',
+      {
+        object: 'process',
+        property: 'env',
+        message: "Use `import { env } from '~/env'` instead to ensure validated types.",
+      },
+    ],
+    'no-restricted-imports': [
+      'error',
+      {
+        name: 'process',
+        importNames: ['env'],
+        message: "Use `import { env } from '~/env'` instead to ensure validated types.",
+      },
+    ],
+  },
+})
+
+export default tseslint.config(
+  {
+    // Globally ignored files
+    ignores: ['**/*.config.js'],
+  },
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      import: importPlugin,
+    },
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      // Typescript Eslint
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+      ],
+      '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+      '@typescript-eslint/no-unnecessary-condition': [
+        'error',
+        {
+          allowConstantLoopConditions: true,
+        },
+      ],
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      // Group & Sort Imports
+      'import/order': [
+        'error',
+        {
+          groups: ['type', 'builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+            },
+            {
+              pattern: 'react-native',
+              group: 'builtin',
+            },
+            {
+              pattern: 'expo',
+              group: 'builtin',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin', 'internal'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreDeclarationSort: true, // handled by import/order
+          ignoreMemberSort: false,
+        },
+      ],
+    },
+  },
+  {
+    linterOptions: { reportUnusedDisableDirectives: true },
+    languageOptions: { parserOptions: { project: true } },
+  },
+)
--- tooling/prettier/lib/index.js
+++ tooling/prettier/lib/index.js
@@ -1,33 +1,20 @@
-// import { fileURLToPath } from "url";
-
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
+  // General
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'lf',
+  //  Plugins
+  // plugins: [
+  // "prettier-plugin-tailwindcss",
+  // ],
   // tailwindConfig: fileURLToPath(
   //   new URL("../../tooling/tailwind/web.ts", import.meta.url),
   // ),
-  tailwindFunctions: ["cn", "cva"],
-  importOrder: [
-    "<TYPES>",
-    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-    "^(next/(.*)$)|^(next$)",
-    "^(expo(.*)$)|^(expo$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "<TYPES>^@splitter",
-    "^@splitter/(.*)$",
-    "",
-    "<TYPES>^[@]",
-    "^[@/]",
-    "",
-    "<TYPES>^[.|..]",
-    "^[../]",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
-};
+  // tailwindFunctions: ["cn", "cva"],
+}
 
-export default config;
+export default config
