17f1662521de4551ebaea3e0a0ab29faece9ed3a
Giuseppe Setem <giuseppe7d@gmail.com>
2024-05-17 18:12:50 -0300

    fix(api): trying to fix esbuild stuff

--- apps/api/esbuild.config.mjs
+++ apps/api/esbuild.config.mjs
@@ -1,12 +1,26 @@
 import esbuild from 'esbuild'
+import authPkg from '../../packages/auth/package.json' assert { type: 'json' }
+import dbPkg from '../../packages/db/package.json' assert { type: 'json' }
+import pkg from './package.json' assert { type: 'json' }
 
 export default esbuild.build({
   logLevel: 'info',
   entryPoints: ['./src/index.ts'],
-  outfile: 'build/index.js',
+  outfile: 'build/index.mjs',
   bundle: true,
   sourcemap: true,
   platform: 'node',
   format: 'esm',
-  external: ['fastify'],
+  external: [
+    ...Object.keys(authPkg.dependencies ?? {}),
+    ...Object.keys(authPkg.peerDependencies ?? {}),
+    ...Object.keys(dbPkg.dependencies ?? {}),
+    ...Object.keys(dbPkg.peerDependencies ?? {}),
+    ...Object.keys(pkg.dependencies ?? {}).filter(
+      (dep) => !dep.startsWith('@splitter'),
+    ),
+    ...Object.keys(pkg.peerDependencies ?? {}).filter(
+      (dep) => !dep.startsWith('@splitter'),
+    ),
+  ],
 })
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,9 @@
 import endpoint from '@/endpoint'
 import env from '@/env'
-import { initAuth } from '@splitter/package-auth'
+import '@splitter/package-auth'
 
 const host = env.API_HOST
 const port = parseInt(env.API_PORT, 10)
-
-initAuth()
 
 endpoint
   .listen({ host, port })
