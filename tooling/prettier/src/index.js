83391b9a008a84bed313b10dd70da0115153b6a6
Giuseppe Setem <giuseppe7d@gmail.com>
2025-05-31 13:41:12 -0300

    refactor(prettier): upgrade prettier version to 3.5 and setup catalog

--- tooling/prettier/src/index.js
+++ tooling/prettier/src/index.js
@@ -1,12 +1,35 @@
+/** @typedef {import("prettier").Options} PrettierOptions */
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+
+/** @type { PrettierConfig | TailwindConfig } */
 const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['cn', 'cva'],
   // General
   printWidth: 80,
   useTabs: false,
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  jsxSingleQuote: true,
   trailingComma: 'all',
   endOfLine: 'lf',
+  // Extra
+  overrides: [
+    {
+      files: '*.json.hbs',
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: '*.js.hbs',
+      options: {
+        parser: 'babel',
+      },
+    },
+  ],
 }
 
 export default config
